41cb43005b7fe7d3ad1b57a938b4a2891c384055
Ton Huynh <kintonhtt@gmail.com>
2026-01-07 12:23:34 -0800

    homepage: add story overview

--- components/home/story-overview.tsx
+++ components/home/story-overview.tsx
@@ -1,3 +1,46 @@
+import Image from "next/image";
+import { DoubleBorderButton } from "../ui/button";
+
 export function StoryOverview() {
-  return <div>StoryOverview</div>;
+  return (
+    <section className="grid min-h-screen grid-cols-1 md:grid-cols-[1fr_2fr_1fr]">
+      <PatternPanel />
+
+      <div className="flex flex-col items-center justify-center gap-8 px-4 text-center">
+        <h1 className="font-titoli text-5xl tracking-wide uppercase">Our Story</h1>
+        <p className="text-lg">
+          Our parents&apos; love story is one of sacrifice, strength, and unbreakable faith.
+        </p>
+
+        <Image
+          src={"/story-overview.png"}
+          alt="Our Story"
+          width={422}
+          height={562}
+          className="border-gold rounded-tl-[100px] rounded-br-[100px] border object-cover"
+        />
+
+        <DoubleBorderButton
+          size={"lg"}
+          className={"font-titoli h-14 p-6 text-xl tracking-wide uppercase"}
+        >
+          Read More
+        </DoubleBorderButton>
+      </div>
+
+      <PatternPanel />
+    </section>
+  );
+}
+
+function PatternPanel() {
+  return (
+    <div className="relative hidden min-h-screen md:block">
+      <div className="bg-wine absolute inset-0" />
+
+      <div className="absolute inset-0 bg-[url('/pattern-lightgold.svg')] bg-size-[400px] bg-center bg-repeat opacity-50" />
+
+      <div className="absolute inset-0 bg-linear-to-b from-black/5 to-black/20" />
+    </div>
+  );
 }
